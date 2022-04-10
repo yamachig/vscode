cf43ae535f348d132377e66009422c06351e8d42
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-04-10 09:32:51 +0200

    term4

--- src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
+++ src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
@@ -333,7 +333,7 @@ class SharedProcessMain extends Disposable {
 			environmentService,
 			logService
 		);
-		await ptyHostService.initialize(); //1235678
+		await ptyHostService.initialize(); //12356789
 
 		// Terminal
 		services.set(ILocalPtyService, this._register(ptyHostService));
